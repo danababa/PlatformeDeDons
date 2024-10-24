1ef06df7693a08418aa99099a40787afc1cf04f6
WalidCh02 <walid.elcheikh@etu.uca.fr>
2024-10-24 11:41:52 +0200

    first commit

---
+++ src/main/java/com/uca/m2/pdd/PddApplication.java
@@ -0,0 +1,13 @@
+package com.uca.m2.pdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PddApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PddApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/uca/m2/pdd/PddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.uca.m2.pdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PddApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
